5c072dce71ef517f6cc71955f9ff7d54d4ad75a1
Dillon Wall <dillonmwall@msn.com>
2024-06-05 17:07:50 +0800

    Prepare for deployment

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
+    baseUrl: process.env.REACT_APP_API_BASE_URL,
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
